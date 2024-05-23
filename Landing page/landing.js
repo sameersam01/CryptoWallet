5c2243f348c30ac7ea812c8332a50cf3528ae16c
Sameer Shrivastava <91184006+sameersam01@users.noreply.github.com>
2024-05-23 22:19:26 +0530

    landing 2 added

--- Landing page/landing.js
+++ Landing page/landing.js
@@ -1,7 +1,8 @@
-var myVar, addNewDiv, var1;
+var myVar, landing2, var1;
 
 function myFunction() {
   myVar = setTimeout(showPage, 1000);
+  landing2=setTimeout(landing2,4000);
 
 }
 
@@ -9,4 +10,9 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("hero").style.display = "flex";
   document.getElementById("slogan").style.display = "flex";
+  
+}
+function landing2(){
+  document.getElementById("hero").innerHTML="";
+  document.location.href="./landing2.html";
 }
